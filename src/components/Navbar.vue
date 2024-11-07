09c99d8623c2cc53359c13b60d2c25277b0bd048
Stanislas Castaybert <perso@stan1712.com>
2024-11-07 18:54:54 +0100

    🚧 Reworked navbar with logo and profile button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,40 @@
 <template>
-  <div>
-    <Menubar :model="navbarItems"/>
-  </div>
+  <Menubar :model="items">
+    <template #start>
+      <div class="my-auto">
+        <Image src="./wigpi_logo.png" height="40" />
+      </div>
+      <h2 class="ml-2 my-0 title">WIGPI</h2>
+    </template>
+    <template #item="{ item, props, hasSubmenu, root }">
+      <a v-ripple class="flex items-center" v-bind="props.action">
+        <i v-if="item.icon" :class="item.icon"></i>
+        <span>{{ item.label }}</span>
+
+        <!-- for lang check -->
+         {{item.label}}
+        <i v-if="item.label" class="ml-auto"></i>
+
+        <Badge v-if="item.badge" :class="{ 'ml-auto': !root}" :value="item.badge" />
+        <i v-if="hasSubmenu" :class="['pi pi-angle-down ml-auto', { 'pi-angle-down': root, 'pi-angle-right': !root }]"></i>
+      </a>
+    </template>
+    <template #end>
+      <div class="flex items-center gap-2">
+        <InputText placeholder="Search" type="text" class="w-32 sm:w-auto" />
+        <Avatar image="/images/avatar/amyelsner.png" shape="circle" />
+      </div>
+    </template>
+  </Menubar>
+  {{locale}}
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
-
 import { useToast } from "primevue/usetoast";
+const toast = useToast();
 
 const router = useRouter();
-const { setLocale, availableLocales, t, locale, getLocaleMessage } = useI18n();
-const toast = useToast();
+const { setLocale, availableLocales, t, getLocaleMessage, locale } = useI18n();
 
 const localesItems: object[] = [];
 availableLocales.forEach((i) => {
@@ -34,19 +57,12 @@ function switchLocale(locale: string) {
   });
 }
 
-const navbarItems = ref([
+const items = [
   {
-    label: t("home"),
+    label: "Home",
     icon: "pi pi-home",
     command: () => {
       router.push("/");
-    },
-  },
-  {
-    label: t("profile"),
-    icon: "pi pi-user",
-    command: () => {
-      router.push("/profile");
     },
   },
   {
@@ -54,9 +70,34 @@ const navbarItems = ref([
     icon: "pi pi-language",
     items: localesItems,
   },
-]);
+  {
+    label: "Projects",
+    icon: "pi pi-search",
+    badge: 3,
+    items: [
+      {
+        label: "Core",
+        icon: "pi pi-bolt",
+        badge: 4,
+      },
+      {
+        label: "Blocks",
+        icon: "pi pi-server",
+      },
+      {
+        separator: true,
+      },
+      {
+        label: "UI Kit",
+        icon: "pi pi-pencil",
+      },
+    ],
+  },
+];
 </script>
 
 <style>
-
+.title {
+  cursor: default;
+}
 </style>
