701281e7241a39356545f5f9ecedc85bd976f3e9
stanislasbdx <s.castaybert@deal.fr>
2024-11-30 02:09:28 +0100

    💄 Added Bricolage Grotesque for accent font

--- app.vue
+++ app.vue
@@ -12,5 +12,16 @@ import NavbarComponent from "./src/components/NavbarComponent.vue";
 <style>
   * {
     font-family: Inter, sans-serif;
+    font-optical-sizing: auto;
+    font-style: normal;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: "Bricolage Grotesque", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 600;
+    font-style: normal;
+    font-variation-settings:
+      "wdth" 100;
   }
 </style>
